18a72e5af4d64ec9187aac01e602b04b1b1e63e9
bangarang <me@alexjrock.com>
2015-05-04 19:48:00 -0500

    Created Categories with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: "Cars")
+Category.create(name: "Houses")
+Category.create(name: "Jobs")
