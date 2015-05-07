8c5bb17fccc73c0da703b85af4c78b86f5d752e9
bangarang <me@alexjrock.com>
2015-05-06 19:48:08 -0500

    Added Password confirmation to tests

--- spec/features/user_creates_category_spec.rb
+++ spec/features/user_creates_category_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 
 feature 'User Signs Up' do
   before do
-    sign_up_with 'John Smith', '12345', 'valid@example.com', 'password'
+    sign_up_with 'John Smith', '12345', 'valid@example.com', 'password', 'password'
   end
-  scenario 'with valid name, zipcode, email and password' do
+  scenario 'with valid name, zipcode, email, password and password_confirmation' do
 
     visit new_category_path
 
@@ -15,12 +15,13 @@ feature 'User Signs Up' do
     expect(page).to have_content('Example Category')
   end
 
-  def sign_up_with(name, zipcode, email, password)
+  def sign_up_with(name, zipcode, email, password, password_confirmation)
     visit new_user_registration_path
     fill_in 'Name', with: name
     fill_in 'Zipcode', with: zipcode
     fill_in 'Email', with: email
     fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password_confirmation
     click_button 'Sign up'
   end
 end
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -3,29 +3,30 @@ require 'rails_helper'
 
 feature 'User Signs Up and Creates Category' do
   scenario 'with valid name, zipcode, email and password' do
-    sign_up_with 'John Smith', '12345', 'valid@example.com', 'password'
+    sign_up_with 'John Smith', '12345', 'valid@example.com', 'password', 'password'
 
     expect(page).to have_content('Logout')
   end
 
   scenario 'with invalid email' do
-    sign_up_with 'John Smith', '12345', 'invalid email', 'password'
+    sign_up_with 'John Smith', '12345', 'invalid email', 'password', 'password'
 
     expect(page).to have_content('Sign up')
   end
 
   scenario 'with blank password' do
-    sign_up_with 'John Smith', '12345','valid@example.com', ''
+    sign_up_with 'John Smith', '12345','valid@example.com', '', ''
 
     expect(page).to have_content('Sign up')
   end
 
-  def sign_up_with(name, zipcode, email, password)
+  def sign_up_with(name, zipcode, email, password, password_confirmation)
     visit new_user_registration_path
     fill_in 'Name', with: name
     fill_in 'Zipcode', with: zipcode
     fill_in 'Email', with: email
     fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password_confirmation
     click_button 'Sign up'
   end
 end
